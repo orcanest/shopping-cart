29e6388512d2f7566ab290494cd991c10dcef9f8
Saeid Behjat <orcastack@gmail.com>
2026-01-01 10:20:24 +0330

    Routing
    create project routes with react-router-dom library

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckOutPage from "./pages/CheckOutPage";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route index element={<Navigate to="/products" replace />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<DetailsPage />} />
+        <Route path="/checkout" element={<CheckOutPage />} />
+        <Route path="/*" element={<PageNotFound />} />
+
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/CheckOutPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CheckOutPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CheckOutPage
