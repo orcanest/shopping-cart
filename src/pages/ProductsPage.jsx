600fc16e9d181ccfe40a3985993ac52060ba4eec
Saeid Behjat <orcastack@gmail.com>
2026-01-01 18:05:22 +0330

    Loading and Search
    add loading and search box

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,20 +1,39 @@
+import { ImSearch } from "react-icons/im";
 import Card from "../components/Card";
+import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
 import Styles from "./ProductsPage.module.css";
+import { useState } from "react";
 
 function ProductsPage() {
   const products = useProducts();
-  console.log(products);
+  const [search, setSearch] = useState("");
+  const searchHandler = () => {
+    
+  };
   return (
-    <div className={Styles.container}>
-      <div className={Styles.products}>
-        {!products.length && <p>Loading ...</p>}
-        {products.map((product) => (
-          <Card key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>Sidebar</div>
-    </div>
+      <div className={Styles.container}>
+        <div className={Styles.products}>
+          {!products.length && <Loader />}
+          {products.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div>Sidebar</div>
+      </div>
+    </>
   );
 }
 
