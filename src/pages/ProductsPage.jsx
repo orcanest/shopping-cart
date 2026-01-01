084ef0b7d48fe8b85e93e47da7f9904dc6c2bcee
Saeid Behjat <orcastack@gmail.com>
2026-01-01 16:48:37 +0330

    Card Component
    create card component and rendering product list

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,9 +1,21 @@
+import Card from "../components/Card";
 import { useProducts } from "../context/ProductContext";
+import Styles from "./ProductsPage.module.css";
 
 function ProductsPage() {
   const products = useProducts();
-  console.log(products)
-  return <div></div>;
+  console.log(products);
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.products}>
+        {!products.length && <p>Loading ...</p>}
+        {products.map((product) => (
+          <Card key={product.id} data={product} />
+        ))}
+      </div>
+      <div>Sidebar</div>
+    </div>
+  );
 }
 
 export default ProductsPage;
