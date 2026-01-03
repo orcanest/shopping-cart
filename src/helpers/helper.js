e02a8ffabeb9104bf12656bd70f5c75144fbff05
Saeid Behjat <orcastack@gmail.com>
2026-01-03 19:02:51 +0330

    Query URL
    - All-category and empty search removing from query URL
    - Get queries and set on initial loading

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -25,4 +25,34 @@ const filterProducts = (products, category) => {
   return filteredProducts;
 };
 
-export { shortenText, searchProducts, filterProducts };
+// All-category and empty search removing from query URL
+const createQueryObject = (currentQuerry, newQuery) => {
+  if (newQuery.category === "all") {
+    const { category, ...rest } = currentQuerry;
+    return rest;
+  }
+  if (newQuery.search === "") {
+    const { search, ...rest } = currentQuerry;
+    return rest;
+  }
+  // if user choosed or search something else of All-category
+  return { ...currentQuerry, ...newQuery };
+};
+
+// Get queries and set on initial loading
+const getInitialQuery = (searchParams) => {
+  const query = {};
+  const category = searchParams.get("category");
+  const search = searchParams.get("search");
+  if (category) query.category = category;
+  if (search) query.search = search;
+  setQuery(query);
+};
+
+export {
+  shortenText,
+  searchProducts,
+  filterProducts,
+  createQueryObject,
+  getInitialQuery,
+};
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
-import { filterProducts, searchProducts } from "../helpers/helper";
+import {
+  createQueryObject,
+  filterProducts,
+  getInitialQuery,
+  searchProducts,
+} from "../helpers/helper";
+import { useSearchParams } from "react-router-dom";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
@@ -13,26 +19,31 @@ function ProductsPage() {
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState({}); // for filter, search, category
 
+  // query string for url
+  const [searchParams, setSearchParams] = useSearchParams();
+
   useEffect(() => {
     setDisplayed(products);
+    setQuery(getInitialQuery(search));
   }, [products]);
 
   useEffect(() => {
+    setSearchParams(query);
+    setSearch(query.search || "")
     let finalProducts = searchProducts(products, query.search);
     finalProducts = filterProducts(finalProducts, query.category);
-
     setDisplayed(finalProducts);
   }, [query]);
 
   const searchHandler = () => {
-    setQuery((query) => ({ ...query, search }));
+    setQuery((query) => createQueryObject(query, { search }));
   };
 
   const categoryHandler = (event) => {
     const { tagName } = event.target;
     const category = event.target.innerText.toLowerCase();
     if (tagName !== "LI") return;
-    setQuery((query) => ({ ...query, category }));
+    setQuery((query) => createQueryObject(query, { category }));
   };
 
   return (
