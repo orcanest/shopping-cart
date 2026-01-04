6809430a680cbc2ee8b58e674f22f756d957e435
Saeid Behjat <orcastack@gmail.com>
2026-01-04 20:17:58 +0330

    basket card
    create and styled

--- src/pages/CheckOutPage.jsx
+++ src/pages/CheckOutPage.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import BasketCard from "../components/BasketCard";
+import { useCart } from "../context/CartContext";
 function CheckOutPage() {
+  const [state, dispatch] = useCart();
+
+  const clickHandler = (type, payload) => dispatch({ type, payload });
+
   return (
     <div>
-      
+      <div>
+        {state.selectedItems.map((product) => (
+          <BasketCard
+            key={product.id}
+            data={product}
+            clickHandler={clickHandler}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default CheckOutPage
+export default CheckOutPage;
