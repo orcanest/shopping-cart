ab837111a3c5f6a93eedb31676951dd7d52beafc
Saeid Behjat <orcastack@gmail.com>
2026-01-04 18:37:52 +0330

    product details page
    create product details page and styled

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { Link, useParams } from "react-router-dom";
+import { useProductDetails } from "../context/ProductContext";
+import Loader from "../components/Loader";
+import { SiOpenproject } from "react-icons/si";
+import { IoMdPricetag } from "react-icons/io";
+import { FaArrowLeft } from "react-icons/fa";
+import styles from "./DetailsPage.module.css";
 
 function DetailsPage() {
+  const { id } = useParams();
+  const productDetails = useProductDetails(+id);
+  if (!productDetails) return <Loader />;
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <img src={productDetails.image} alt={productDetails.title} />
+      <div className={styles.information}>
+        <h3 className={styles.title}>{productDetails.title}</h3>
+        <p className={styles.description}>{productDetails.description}</p>
+        <p className={styles.category}>
+          <SiOpenproject />
+          {productDetails.category}
+        </p>
+        <div>
+          <span className={styles.price}>
+            <IoMdPricetag />
+            {productDetails.price} $
+          </span>
+          <Link to="/products" >
+            <FaArrowLeft />
+            <span>Back to Shop</span>
+          </Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default DetailsPage
+export default DetailsPage;
