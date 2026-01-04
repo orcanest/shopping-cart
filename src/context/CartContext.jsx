063629829468a83199e6bbcb58dcc28cdb440cc8
Saeid Behjat <orcastack@gmail.com>
2026-01-04 17:10:26 +0330

    other actions for reducer
    REMOVE_ITEM
    INCREASE
    DECREASE
    CHECKOUT

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -11,13 +11,47 @@ const initialState = {
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_ITEM":
-      if (!state.selectedItems.find((item) => item.id == action.payload.id)) {
+      if (!state.selectedItems.find((item) => item.id === action.payload.id)) {
         state.selectedItems.push({ ...action.payload, quantity: 1 });
       }
       return {
         ...state,
         ...sumProducts(state.selectedItems),
         checkout: false,
+      };
+    case "REMOVE_ITEM":
+      const newSelectedItems = state.selectedItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+      return {
+        ...state,
+        selectedItems: [...newSelectedItems],
+        ...sumProducts(newSelectedItems),
+      };
+    case "INCREASE":
+      const increaseIndex = state.selectedItems.findIndex(
+        (item) => item.id === action.id
+      );
+      state.selectedItems[increaseIndex].quantity++;
+      return {
+        ...state,
+        ...sumProducts(state.selectedItems),
+      };
+    case "DECREASE":
+      const decreaseindex = state.selectedItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      state.selectedItems[decreaseindex].quantity--;
+      return {
+        ...state,
+        ...sumProducts(state.selectedItems),
+      };
+    case "CHECKOUT":
+      return {
+        selectedItems: [],
+        itemsCounter: 0,
+        total: 0,
+        checkout: true,
       };
     default:
       throw new Error("Invalid Action!");
