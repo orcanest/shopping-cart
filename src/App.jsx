d84748ede30d6dbb127a9eb7e278f32bfa52862f
Saeid Behjat <orcastack@gmail.com>
2026-01-04 18:05:37 +0330

    layout
    adding layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+
+import Layout from "./layout/Layout";
 import ProductsPage from "./pages/ProductsPage";
 import DetailsPage from "./pages/DetailsPage";
 import CheckOutPage from "./pages/CheckOutPage";
 import PageNotFound from "./pages/PageNotFound";
 import ProductProvider from "./context/ProductContext";
 import CartProvider from "./context/CartContext";
+
 function App() {
   return (
     <div>
       <CartProvider>
         <ProductProvider>
-          <Routes>
-            <Route index element={<Navigate to="/products" replace />} />
-            <Route path="/products" element={<ProductsPage />} />
-            <Route path="/products/:id" element={<DetailsPage />} />
-            <Route path="/checkout" element={<CheckOutPage />} />
-            <Route path="/*" element={<PageNotFound />} />
-          </Routes>
+          <Layout>
+            <Routes>
+              <Route index element={<Navigate to="/products" replace />} />
+              <Route path="/products" element={<ProductsPage />} />
+              <Route path="/products/:id" element={<DetailsPage />} />
+              <Route path="/checkout" element={<CheckOutPage />} />
+              <Route path="/*" element={<PageNotFound />} />
+            </Routes>
+          </Layout>
         </ProductProvider>
       </CartProvider>
     </div>
